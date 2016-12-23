3eefb16acdc0336dbcc34606abe785e1ca492657
Steffen Oppel <steffen.oppel@gmail.com>
2016-12-23 10:24:21 +0000

    added extended batchUD function that returns overlap indices

--- seabird_index.r
+++ seabird_index.r
@@ -49,3 +49,96 @@ return(report)
 
   }
 
+
+
+
+
+########## SEABIRD SPATIAL OVERLAP INDEX  ######################################################################################################
+
+## STEFFEN OPPEL 2016
+## This function is based on the marine IBA 'batchUD' function (Lascelles et al. 2016)
+## Modified to use adehabitatHR and calculate overlap indices
+## returns now a list with the SpatialPolygonsDataFrame and a data.frame with the overlap indices
+
+
+batchUDOL <- function(DataGroup, Scale = 50, UDLev = 50)
+    {
+    require(sp)
+    require(maptools)
+    require(rgdal)
+    require(adehabitatHR)
+    require(geosphere)
+
+    if(!"Latitude" %in% names(DataGroup)) stop("Latitude field does not exist")
+    if(!"Longitude" %in% names(DataGroup)) stop("Longitude field does not exist")
+    if(!"ID" %in% names(DataGroup)) stop("ID field does not exist")
+
+    if(class(DataGroup)!= "SpatialPointsDataFrame")     ## convert to SpatialPointsDataFrame and project
+    {
+    mid_point<-data.frame(centroid(cbind(DataGroup$Longitude, DataGroup$Latitude)))
+    DataGroup.Wgs <- SpatialPoints(data.frame(DataGroup$Longitude, DataGroup$Latitude), proj4string=CRS("+proj=longlat + datum=wgs84"))
+    DgProj <- CRS(paste("+proj=laea +lon_0=", mid_point$lon, " +lat_0=", mid_point$lat, sep=""))
+    DataGroup.Projected <- spTransform(DataGroup.Wgs, CRS=DgProj)
+    DataGroup <- SpatialPointsDataFrame(DataGroup.Projected, data = DataGroup)
+    }else{DgProj<-DataGroup@proj4string}
+
+    DataGroup$X <- DataGroup@coords[,1]
+    DataGroup$Y <- DataGroup@coords[,2]
+
+    UIDs <- unique(DataGroup$ID)
+
+
+##### USE NEW kernelUD function to bypass loop over individuals ####
+
+TripCoords<-SpatialPointsDataFrame(DataGroup, data=DataGroup@data[,9:10])		## this only works if 'ID' is in those two columns!!
+TripCoords@data$bird_id<-NULL
+Ext <- (min(coordinates(TripCoords)[,1]) + 3 * diff(range(coordinates(TripCoords)[,1])))
+if(Ext < (Scale * 1000 * 2)) {BExt <- ceiling((Scale * 1000 * 3)/(diff(range(coordinates(TripCoords)[,1]))))} else {BExt <- 3}
+
+KDE.Surface <- adehabitatHR::kernelUD(TripCoords, h=(Scale * 1000), grid=1000, extent=BExt, same4all=FALSE)		## newer version needs SpatialPoints object and id no longer required in adehabitatHR, also removed 'extent' as it caused problems
+KDE.Sp <- adehabitatHR::getverticeshr(KDE.Surface, lev = UDLev)	
+
+    UIDs <- names(which(table(DataGroup$ID)>5))
+    KDE.Sp@proj4string <- DgProj
+    KDE.Wgs <- spTransform(KDE.Sp, CRS=CRS("+proj=longlat +ellps=WGS84"))
+    Tbl <- data.frame(Name_0 = rep(1, length(UIDs)), Name_1 = 1:length(UIDs), ID = UIDs)
+    row.names(Tbl) <- UIDs
+    KDE.Spdf <- SpatialPolygonsDataFrame(KDE.Sp, data=Tbl)
+
+    plot(KDE.Spdf, border=factor(UIDs))
+      
+##### OVERLAYS IN THE polyCount FUNCTION WILL NOT WORK IF THE POLYGONS CONTAIN HOLES OR ARE ORPHANED
+      ## simple fix to remove holes from polygon object
+      va90a <- spChFIDs(KDE.Spdf, paste(KDE.Spdf$Name_0, KDE.Spdf$Name_1, KDE.Spdf$ID, sep = ""))
+      va90a <- va90a[, -(1:4)]
+      va90_pl <- slot(va90a, "polygons")
+      va90_pla <- lapply(va90_pl, checkPolygonsHoles)
+      p4sva <- CRS(proj4string(va90a))
+      vaSP <- SpatialPolygons(va90_pla, proj4string = p4sva)
+      va90b <- SpatialPolygonsDataFrame(vaSP, data = as(va90a, "data.frame"))   ### this returns an empty data frame
+      va90b@data<-KDE.Spdf@data                                               ### this adds the original data back into the data frame - may not work if entire polygons are removed
+      
+      
+    #return(va90b)     ## R does not allow return of multiple objects, hence combined in list below
+
+##### CALCULATE VARIOUS OVERLAP INDICES #####
+
+olInd<-data.frame(method=c("HR", "PHR", "VI", "BA", "UDOI", "HD"), mean_index=0, n_comps=0)
+for(ix in c("HR", "PHR", "VI", "BA", "UDOI", "HD")){
+OL1<-adehabitatHR::kerneloverlaphr(KDE.Surface, method = ix,percent= UDLev, conditional = FALSE)
+
+## CALCULATE MEAN BUT REMOVE DIAGONAL
+#str(OL1)
+OL1<-as.data.frame(melt(OL1))
+OL1<-OL1[!(OL1$X1==OL1$X2),]
+olInd$mean_index[olInd$method==ix]<-mean(OL1$value)
+olInd$n_comps[olInd$method==ix]<-dim(OL1)[1]
+}
+
+####
+outlist<-list("UDpolygons"=va90b,"OverlapIndex"=olInd)
+return(outlist)
+
+
+    }
+
